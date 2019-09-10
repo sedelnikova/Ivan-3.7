70b3ad48fed2c381c7581ba941ccd3b5f6d4179b
sedelnikova <54905384+sedelnikova@users.noreply.github.com>
2019-09-10 15:12:41 +0300

    Добавьте файлы проекта.

---
+++ Иван 3.7/Иван 3.7.cpp
@@ -0,0 +1,20 @@
+﻿// Иван 3.7.cpp : Этот файл содержит функцию "main". Здесь начинается и заканчивается выполнение программы.
+//
+
+#include <iostream>
+#include <cmath>
+using namespace std;
+int main()
+{
+	setlocale(LC_ALL, "Russian");
+	float A, O, E;
+
+	cout << "Введите, пожалуйста, A =";
+	cin >> A;
+	O = A * A * A;
+	E = O * O;
+	O = E * E * O;
+
+	cout << "A= " << O << endl;
+	return 0;
+}
